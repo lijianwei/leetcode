1e6b735a45213d22d2c179973df49fe0b37b2d1c
lijianwei <aaaajw@163.com>
2018-11-06 16:09:15 +0800

    添加字典版本，利用键值对字典来存储，一次遍历，空间换时间

--- two_sum.py
+++ two_sum.py
@@ -5,18 +5,34 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        lens=len(nums)
-        i=0
-        while i <(lens-1) :
-            j=i+1
-            while j<=(lens-1):
-                if(nums[i]+nums[j]) == target:
-                    return [i,j]
-                j=j+1
-            i=i+1
+        dic_func(nums, target)
+
+
+
+def baoli_func(nums, target):
+    lens = len(nums)
+    i = 0
+    while i < (lens - 1):
+        j = i + 1
+        while j <= (lens - 1):
+            if (nums[i] + nums[j]) == target:
+                return [i, j]
+            j = j + 1
+        i = i + 1
+
+
+def dic_func(nums, target):
+    zdict = {}  # 利用键值对字典来空间换时间
+    index = 0  # 索引从0开始记起
+    for i in nums:
+        j = target-i
+        if j in zdict:
+            return[zdict[j], index]
+        # 如果没有则把这次寻找匹配的i和i的index存入zdict中
+        zdict[i] = index
+        index = index+1
 
 
 if __name__ == '__main__':
-    solution=Solution()
-    result=solution.twoSum([1,2,3,4,5,6], 9)
+    result = dic_func([2,7,11,15], 9)
     print(result)
